e822f93818899eec5570d513111d2cfa940105f6
MuhammadHuzaifaSheikh <huzaifa900shaikh@gmail.com>
2025-03-09 04:35:32 +0500

    updated

--- index.js
+++ index.js
@@ -323,7 +323,7 @@ app.get('/', (req, res) => {
         <h1>Instagram Business Login Demo</h1>
         <p>Current callback URL: ${baseUrl}/auth/instagram/callback</p>
         <a href="${loginUrl}">
-          <button>Login with Instagram Business</button>
+          <button>Login with Instagram Business Account</button>
         </a>
       </body>
     </html>
